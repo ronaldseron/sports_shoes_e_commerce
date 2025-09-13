149e37e47543a659f134e92b85464f1a3c6d8e9b
Ronse11 <seronronalddalida@gmail.com>
2025-09-13 15:25:37 +0800

    feat: add MainLayout and wrap routes for consistent layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,26 @@
 // import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import MainLayout from './layout/MainLayout';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import About from './pages/About';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 
 function App() {
 
     return (
         <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/shop" element={<Shop />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-            <Footer />
+            <MainLayout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </MainLayout>
         </Router>
     );
 }
