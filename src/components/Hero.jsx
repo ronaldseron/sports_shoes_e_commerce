fe871efc7f25f5d5084e8dd5a5136ee92e1e704e
Ronse11 <seronronalddalida@gmail.com>
2025-09-13 15:24:23 +0800

    chore: updated height to fixed px

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
+import { useEffect, useRef } from 'react';
 import heroShoe from '../assets/images/hero-shoe12.png'
 
 const Hero = () => {
-  return (
-    <section className=" w-full lg:min-h-[500px] h-[85dvh]">
-        <div className='h-full flex items-center justify-center border bg-bg-primary border-gray-200 overflow-hidden'>
-            <div className='w-1/2 p-16'>
-                <div className=' text-white z-10'>
-                    <h3 className="text-3xl font-family-primary tracking-wide mb-3">— Your Game Starts Here</h3>
-                    <h1 className=" font-family-primary text-7xl text-balance mb-2">UNLEASH YOUR POTENTIAL</h1>
-                    <p className="text-lg font-light mb-10">Discover the latest Nike sports shoes engineered for speed, comfort, and style. Elevate your game on and off the field.</p>
-                    <a href="/shop" className="bg-white text-ft-primary px-6 py-3 border border-black/25">Shop Now</a>
+    return (
+        <section className=" w-full max-w-site h-150">
+            <div className='h-full flex items-center justify-center border bg-bg-primary border-gray-200 overflow-hidden'>
+                <div className='w-1/2 p-16'>
+                    <div className=' text-white z-10'>
+                        <h3 className="text-2xl font-family-primary tracking-wide mb-2"
+                        style={{ fontSize: 'clamp(0.7rem, 1.8vw, 1.5rem)' }}>— Your Game Starts Here</h3>
+                        <h1 className=" font-family-primary text-7xl tracking-wide text-balance mb-1"
+                        style={{ fontSize: 'clamp(2.5rem, 6vw, 5rem)' }}>UNLEASH POTENTIAL</h1>
+                        <p className="text-lg font-light mb-10"
+                        style={{ fontSize: 'clamp(12px, 1.3vw, 18px)' }}>Discover the latest Nike sports shoes engineered for speed, comfort, and style. Elevate your game on and off the field.</p>
+                        <a href="/shop" className="py-2 px-5 bg-white text-ft-primary text-lg border border-black/70">To check latest arrivals</a>
+                    </div>
+                </div>
+                <div className='w-1/2 h-full relative'>
+                    <div className='relative w-full h-full z-10 backdrop-blur-3xl grid place-content-center'>
+                        <img 
+                            src={heroShoe} 
+                            alt="Hero Page" 
+                            className=" object-cover "
+                        />
+                    </div>
                 </div>
             </div>
-            <div className='w-1/2 h-full relative'>
-                {/* <div className='absolute top-1/2 -translate-y-1/2 w-[15vw] h-[50vh] rounded-r-full bg-black'></div>
-                <div className='absolute top-0 right-0 -translate-y-1/2 w-[20vw] h-[60vh] rounded-l-full bg-black'></div>
-                <div className='absolute bottom-0 right-0 w-[10vw] h-[60vh] rounded-l-full bg-blue-900'></div> */}
-                <div className='relative w-full h-full z-10 backdrop-blur-3xl grid place-content-center'>
-                    <img 
-                        src={heroShoe} 
-                        alt="Hero Page" 
-                        className=" object-cover "
-                    />
-                </div>
-            </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Hero
