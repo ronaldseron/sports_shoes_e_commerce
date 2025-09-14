e12c9c294ae09ed4e4c66de3471b98116efb9a7f
Ronse11 <seronronalddalida@gmail.com>
2025-09-14 16:41:02 +0800

    feat: add marquee for text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import Shop from './Shop'
-import Blog from './Blog'
-import Contact from './Contact'
+import Category from './Category'
+import Spotlight from '../components/Spotlight'
 
 const Home = () => {
   return (
-    <>
-        <Hero />
-            <div>
-                Home
-            </div>
-        <Shop />
-        <Blog />
-        <Contact />
+    <>  
+        <div>
+          <Hero />
+          <div className="w-full h-15 bg-ft-primary font-family-primary text-white flex items-center justify-center text-2xl">
+              <h1>STEP UP // CHUCHU // BLABAL // NYEHEHE // WHAT // STEP UP // CHUCHU // BLABLA // NYEHEHE // WHAT</h1>
+          </div>
+        </div>
+        <Category />
+        <Spotlight />
     </>
   )
 }
