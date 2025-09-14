2b2c576f38fba0f725e372d50bf3de91b0155658
Ronse11 <seronronalddalida@gmail.com>
2025-09-14 16:41:52 +0800

    chore: add imported Category

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import MainLayout from './layout/MainLayout';
 import Home from './pages/Home';
-import Shop from './pages/Shop';
+import Category from './pages/Category';
 import About from './pages/About';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
@@ -17,7 +17,7 @@ function App() {
             <MainLayout>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/shop" element={<Category />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/blog" element={<Blog />} />
                     <Route path="/contact" element={<Contact />} />
