72f2940bd7ee36cd42c7134a02604bdebb08d707
Ronse11 <seronronalddalida@gmail.com>
2025-09-15 16:06:07 +0800

    chore: import new sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,31 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Category from './Category'
 import Spotlight from '../components/Spotlight'
+import Featured from '../components/Featured'
+import WhatSetsApart from '../components/WhatSetsApart'
+import Lifestyle from '../components/Lifestyle'
 
 const Home = () => {
   return (
     <>  
         <div>
           <Hero />
-          <div className="w-full h-15 bg-ft-primary font-family-primary text-white flex items-center justify-center text-2xl">
-              <h1>STEP UP // CHUCHU // BLABAL // NYEHEHE // WHAT // STEP UP // CHUCHU // BLABLA // NYEHEHE // WHAT</h1>
+          <div className="w-full h-15 bg-ft-primary font-family-primary text-white flex items-center text-2xl overflow-hidden relative">
+            <div className="marquee whitespace-nowrap flex">
+              <span>
+                STEP UP // CHUCHU // BLABAL // NYEHEHE // WHAT // STEP UP // CHUCHU // BLABLA // NYEHEHE // WHAT
+              </span>
+              <span className="ml-8">
+                STEP UP // CHUCHU // BLABAL // NYEHEHE // WHAT // STEP UP // CHUCHU // BLABLA // NYEHEHE // WHAT
+              </span>
+            </div>
           </div>
         </div>
         <Category />
         <Spotlight />
+        <Featured />
+        <WhatSetsApart />
+        <Lifestyle />
     </>
   )
 }
