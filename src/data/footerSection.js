aeba638b3cafe6aa5d7a097377bbc91d26e6bd0a
Ronse11 <seronronalddalida@gmail.com>
2025-09-17 16:46:02 +0800

    chore: add footerSection data

---
+++ src/data/footerSection.js
@@ -0,0 +1,28 @@
+const footerSection = [
+	{
+		title: "SHOP BY SPORT",
+		links: [
+			"Basketball Shoes", "Football Shoes", "Volleyball Shoes", "Tennis Shoes", "Badminton Shoes", "Baseball Shoes", "Golf Shoes"
+		]
+	},
+	{
+		title: "COMPANY",
+		links: [
+			"Privacy Policy", "Terms $ Conditions", "Refund Policy"
+		]
+	},
+	{
+		title: "LEGAL",
+		links: [
+			"Privacy Policy", "Terms of Service", "Refund Policy"
+		]
+	},
+	{
+		title: "CUSTOMER SERVICE",
+		links: [ 
+			"Shipping & Delivery", "Returns & Exchanges", "Size Guide", "FAQs", "Contact Us"
+		]
+	}
+];
+
+export default footerSection;
