71870a267372ff7b40a058760b4e8e38c4469f0a
Ronse11 <seronronalddalida@gmail.com>
2025-09-17 16:46:37 +0800

    chore: add newsSEction data

---
+++ src/data/newsSection.js
@@ -0,0 +1,40 @@
+const newsSection = [
+	{
+		title: "BASKETBALL",
+		links: [
+			"NBA Official News", "ESPN Basketball", "Bleacher Report Basketball"
+		]
+	},
+	{
+		title: "FOOTBAL",
+		links: [
+			"FIFA News", "ESPN Football", "Goal.com"
+		]
+	},
+	{
+		title: "VOLLEYBALL",
+		links: [
+			"FIVB Official News", "VolleyMob"
+		]
+	},
+	{
+		title: "TENNIS",
+		links: [ 
+			"ATP Tour News", "WTA Tennis"
+		]
+	},
+	{
+		title: "BADMINTON",
+		links: [ 
+			"Badminton World Federation", "Badzine"
+		]
+	},
+	{
+		title: "BASEBALL",
+		links: [ 
+			"MLB News", "ESPN Baseball"
+		]
+	}
+];
+
+export default newsSection;
