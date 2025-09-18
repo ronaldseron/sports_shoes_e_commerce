be0a7fbc7ecdba2d8acfc1fdc82dfe52bfc321e4
Ronse11 <seronronalddalida@gmail.com>
2025-09-18 15:08:10 +0800

    chore: updated height

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import heroShoe from '../assets/images/hero-shoe18.png'
 
 const Hero = () => {
     return (
-        <section className=" w-full max-w-site h-140">
+        <section className=" w-full max-w-site h-150">
             <div className='h-full flex items-center justify-center bg-bg-primary overflow-hidden'>
                 <div className='w-1/2 p-16'>
                     <div className=' text-white z-10'>
@@ -14,7 +14,10 @@ const Hero = () => {
                         style={{ fontSize: 'clamp(2.5rem, 6vw, 5rem)' }}>UNLEASH POTENTIAL</h1>
                         <p className="text-lg font-light mb-10"
                         style={{ fontSize: 'clamp(12px, 1.3vw, 18px)' }}>Discover the latest Nike sports shoes engineered for speed, comfort, and style. Elevate your game on and off the field.</p>
-                        <a href="/shop" className="py-2 px-5 bg-white text-ft-primary text-lg border border-black/70">To check latest arrivals</a>
+                        <div>
+                            <a href="/shop" className="py-2 px-5 bg-white text-ft-primary text-lg border border-black/70 mr-2">To check latest arrivals</a>
+                            <a href="/shop" className="py-2 px-5 text-white text-lg underline">Trending shoes for each sport</a>
+                        </div>
                     </div>
                 </div>
                 <div className='w-1/2 h-full relative'>
