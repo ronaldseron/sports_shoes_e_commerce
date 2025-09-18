a80f12625b5547a09a8c662e63b3d031c779ef81
Ronse11 <seronronalddalida@gmail.com>
2025-09-18 15:12:37 +0800

    chore: add imported Marquee

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Marquee from '../components/Marquee'
 import Category from './Category'
 import Spotlight from '../components/Spotlight'
 import Featured from '../components/Featured'
@@ -10,19 +11,8 @@ import SportNews from '../components/SportNews'
 const Home = () => {
   return (
     <>  
-        <div>
-          <Hero />
-          <div className="w-full h-15 bg-ft-primary font-family-primary text-white flex items-center text-2xl overflow-hidden relative">
-            <div className="marquee whitespace-nowrap flex">
-              <span>
-                STEP UP // CHUCHU // BLABAL // NYEHEHE // WHAT // STEP UP // CHUCHU // BLABLA // NYEHEHE // WHAT
-              </span>
-              <span className="ml-8">
-                STEP UP // CHUCHU // BLABAL // NYEHEHE // WHAT // STEP UP // CHUCHU // BLABLA // NYEHEHE // WHAT
-              </span>
-            </div>
-          </div>
-        </div>
+        <Hero />
+        <Marquee />
         <Category />
         <Spotlight />
         <Featured />
