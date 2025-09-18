a99f54a2edc1a5778cd089ce52a0253cd974ee22
Ronse11 <seronronalddalida@gmail.com>
2025-09-18 15:04:53 +0800

    feat: add Marquee for Home page

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Marquee = () => {
+  return (
+        <div className="w-full h-13 bg-ft-primary font-family-primary text-white flex items-center text-xl overflow-hidden relative">
+          <div className="marquee whitespace-nowrap ">
+            <h1>
+              FREE SHIPPING <span className="text-yellow-300 mx-6">//</span> NEW ARRIVALS <span className="text-yellow-300 mx-6">//</span> LIMITED EDITION DROPS <span className="text-yellow-300 mx-6">//</span> MEMBERS GET EXTRA 10% OFF <span className="text-yellow-300 mx-6">//</span> SHOP TOP ATHLETE PICKS <span className="text-yellow-300 mx-6">//</span> FAST DELIVERY <span className="text-yellow-300 mx-6">//</span> EASY RETURNS <span className="text-yellow-300 mx-6">//</span> SUMMER SALE <span className="text-yellow-300 mx-6">//</span>
+            </h1>
+            <h1 className="">
+              FREE SHIPPING <span className="text-yellow-300 mx-6">//</span> NEW ARRIVALS <span className="text-yellow-300 mx-6">//</span> LIMITED EDITION DROPS <span className="text-yellow-300 mx-6">//</span> MEMBERS GET EXTRA 10% OFF <span className="text-yellow-300 mx-6">//</span> SHOP TOP ATHLETE PICKS <span className="text-yellow-300 mx-6">//</span> FAST DELIVERY <span className="text-yellow-300 mx-6">//</span> EASY RETURNS <span className="text-yellow-300 mx-6">//</span> SUMMER SALE <span className="text-yellow-300 mx-6">//</span>
+            </h1>
+          </div>
+        </div>
+  )
+}
+
+export default Marquee
