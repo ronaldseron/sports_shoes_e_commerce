54765aad647cee2d0e1ecf463411ea82e63e7996
Ronse11 <seronronalddalida@gmail.com>
2025-09-18 15:07:26 +0800

    chore: add payment and social media logos and icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import footerSection from '../data/footerSection'
+import { footerSection, paymentOptions, socialMedia } from '../data/footerSection'
 
 
 const Footer = () => {
 	return (
-		<footer className="max-w-site mx-auto bg-gray-50 text-primary px-16 py-16 flex flex-col gap-2 z-50">
+		<footer className="max-w-site mx-auto bg-gray-50 text-primary px-16 py-16 flex flex-col gap-6 z-50">
 			<div className="grid grid-cols-4">
 				{footerSection.map((section, idx) => (
 					<nav key={idx} aria-label={section.title}>
@@ -17,6 +17,34 @@ const Footer = () => {
 							))}
 						</ul>
 					</nav>
+				))}
+			</div>
+			<div className="grid grid-cols-2">
+				{paymentOptions.map((payment, idx) => (
+					<div key={idx}>
+						<h3 className="font-family-primary text-lg mb-6">{payment.title}</h3>
+						<div className="flex items-center gap-6">
+							{payment.links.map((link, idx) => (
+								<div key={idx} className="flex items-center gap-2">
+									<img src={link.image} alt={link.name} className="w-14"/>
+									<h5 className="text-lg font-light">{link.name}</h5>
+								</div>
+							))}
+						</div>
+					</div>
+				))}
+				{socialMedia.map((social, idx) => (
+					<div key={idx}>
+						<h3 className="font-family-primary text-lg mb-6">{social.title}</h3>
+						<div className="flex items-center gap-6">
+							{social.links.map((link, idx) => (
+								<div key={idx} className="flex items-center gap-2">
+									<img src={link.image} alt={link.name} className="w-9"/>
+									<h5 className="text-lg font-light">{link.name}</h5>
+								</div>
+							))}
+						</div>
+					</div>
 				))}
 			</div>
 		</footer>
