e6b56fb66b5378d568e129b3f10495fcbde1df27
Ronse11 <seronronalddalida@gmail.com>
2025-09-18 15:06:18 +0800

    chore: updated card for clean look

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -8,12 +8,46 @@ const Featured = () => {
             <h3 className="font-family-primary text-2xl mb-2">THIS SEASONS'S TOP PICKS</h3>
             <h1 className="font-family-primary text-6xl tracking-wider">DISCOVER TRENDING SPORTS GEAR PICKED BY TOP ATHLETES</h1>
         </div>
-        <div className="grid grid-cols-6 h-70 gap-2 px-2">
+        <div className="grid grid-cols-4 px-8 gap-8">
             {FeaturedShoes.map((shoe, idx) => (
-                <div key={idx} className={`h-full flex items-center justify-center bg-gray-50 p-10`}>
-                    <img src={shoe.image} alt={`shoe-${idx}`} />
+                <div key={idx} className={` flex flex-col`}>
+                    <div className=" flex items-center p-8 bg-gradient-to-r from-gray-200 via-gray-50 to-gray-200">
+                        <img src={shoe.image} alt={`shoe-${idx}`} />
+                    </div>
+                    <div className="px-2 py-4 rounded-t-2xl">
+                        <h1 className="text-2xl font-semibold mb-4">Air Glide Pro</h1>
+                        <p className="opacity-70">Bounce Higher, land softer. Dominatethe court with responsive cushioning.</p>
+                        <div className="flex items-center justify-between mt-8">
+                            <div>
+                                <h5 className="text-xs font-semibold">PRICE</h5>
+                                <h1 className="text-lg font-bold">1000</h1>
+                            </div>
+                            <button className="px-6 py-3 font-semibold bg-gray-100 rounded-lg">Add to cart</button>    
+                        </div>
+                    </div>
                 </div>
             ))}
+        </div>
+        <div className="bg-tertiary py-34 flex items-center mt-34">
+            <div className="px-16 mb-6">
+                <h3 className="font-family-primary text-2xl mb-2">WHAT OUR CUSTOMERS SAY</h3>
+                <h1 className="font-family-primary text-6xl tracking-wider">"DISCOVER TRENDING SPORTS GEAR PICKED BY TOP ATHLETES"</h1>
+                <div className="flex items-center gap-2 mt-4">
+                    <div className="w-12 h-12 grid place-content-center bg-black text-white rounded-full">U</div>
+                    <div>
+                        <h1 className="font-semibold">Ronald Seron</h1>
+                        <h3>SS satisfied customer</h3>
+                    </div>
+                </div>
+                <div className="flex items-center gap-4 mt-12">
+                    <button className="bg-white h-10 w-10 rounded-full">
+                        <i className="bx bx-chevron-left text-2xl mt-1"></i>
+                    </button>
+                    <button className="bg-white h-10 w-10 rounded-full">
+                        <i className="bx bx-chevron-right text-2xl mt-1"></i>
+                    </button>
+                </div>
+            </div>
         </div>
     </section>
   )
