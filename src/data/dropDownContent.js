0b2c1bbe94cd8810b87fa50bcfd71e819b2ee2e5
Ronse11 <seronronalddalida@gmail.com>
2025-09-19 16:34:48 +0800

    chore: add dropdown contents/links

---
+++ src/data/dropDownContent.js
@@ -0,0 +1,52 @@
+const dropDownContent = {
+    Sports: [
+        {
+            title: "SPORTS OVERVIEW",
+            links: [
+                { name: "All Sport Shoes", to: "/all-sport-shoes" },
+                { name: "Men's Sport Shoes", to: "/mens-sport-shoes" },
+                { name: "Women's Sport Shoes", to: "/womens-sport-shoes" },
+                { name: "Kids' Sport Shoes", to: "/kids-sport-shoes" },
+                { name: "New Arrivals", to: "/new-arrivals" },
+                { name: "Best Sellers", to: "/best-sellers" },
+                { name: "Training Shoes", to: "/training-shoes" },
+            ]
+        },
+        {
+            title: "SPORTS",
+            links: [
+                { name: "Basketball", to: "/basketball" },
+                { name: "Football", to: "/football" },
+                { name: "Volleyball", to: "/volleyball" },
+                { name: "Tennis", to: "/football" },
+                { name: "Baseball", to: "/baseball" },
+                { name: "Badminton", to: "/badminton" },
+            ]
+        },
+        {
+            title: "COLLECTIONS",
+            links: [
+                { name: "Limited Edition", to: "/limited-edition" },
+                { name: "Eco-Friendly Shoes", to: "/eco-friendly" },
+                { name: "Performance Series", to: "/performance-series" },
+                { name: "Street Style", to: "/street-style" },
+                { name: "Classic Collection", to: "/classic-collection" },
+                { name: "Seasonal Picks", to: "/seasonal-picks" },
+                { name: "Collaboration Series", to: "/collaborations" }
+            ]
+        }
+    ],
+    About: [
+        { 
+            title: "WE ARE SPORTSHOES",
+            link: { name: "About us", to: "/about"}
+        },
+        {
+            title: "SPORTS NEWS",
+            link: { name: "Newsroom", to: "/newsroom" }
+            
+        }
+    ]
+}
+
+export default dropDownContent;
