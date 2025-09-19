9f1fd27612cf9c68f3af2e87de40335829516a78
Ronse11 <seronronalddalida@gmail.com>
2025-09-19 16:35:21 +0800

    chore: updated shoes images

--- src/data/featuredShoes.js
+++ src/data/featuredShoes.js
@@ -1,23 +1,32 @@
+import Feat1 from '../assets/images/feat-1.png'
 import Feat2 from '../assets/images/feat-2.png'
 import Feat3 from '../assets/images/feat-3.png'
 import Feat4 from '../assets/images/feat-4.png'
 
 const FeaturedShoes = [
     {
-        image: Feat2,
-        bgColor: "bg-[#7d7172]"
+        title: "SwiftLite 5",
+        desc: "Featherlight comfort engineered for speed and agility.",
+        price: "$134",
+        image: Feat1,
     },
     {
+        title: "Air Glide Pro",
+        desc: "Bounce Higher, land softer. Dominatethe court with responsive cushioning.",
+        price: "$180",
         image: Feat4,
-        bgColor: "bg-[#6fa6ae]"
     },
     {
+        title: "Predator Strike",
+        desc: "Precision touch and speed control with pro-level firm ground grip.",
+        price: "$200",
         image: Feat3,
-        bgColor: "bg-[#f19a75]"
     },
     {
+        title: "Sky Jump X",
+        desc: "Lightweight design with advanced shock support for safe, powerful leaps.",
+        price: "$150",
         image: Feat2,
-        bgColor: "bg-[#f19a75]"
     },
 ];
 
